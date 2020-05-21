153c54b8c75d5d479bafc8ddd433c9143c6792af
David Fudge <rkstar@mac.com>
2020-05-21 19:55:20 -0400

    add paging width snap to center for words

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import {View, ScrollView, StyleSheet} from 'react-native';
 
 import WordCard from './WordCard';
 
 const wordsList = ['one', 'two', 'three', 'box', 'mix', 'fox', 'up', 'are', 'four', 'five', 'lunch'];
 
-const App = () => (
-  <View style={styles.container}>
-    <ScrollView contentContainerStyle={styles.scrollView} horizontal>
-      {wordsList.map(word => (
-        <WordCard key={word} word={word} />
-      ))}
-    </ScrollView>
-  </View>
-);
+const App = () => {
+  const [viewWidth, setViewWidth] = useState<number>(0);
+  const wordListContainer = useRef<View | null>();
+
+  useEffect(() => {
+    const measureView = () => wordListContainer.current?.measure((_, __, width) => setViewWidth(width));
+    measureView();
+    window.addEventListener('resize', measureView);
+    return () => window.removeEventListener('resize', measureView);
+  }, []);
+
+  return (
+    <View style={styles.container} ref={ref => (wordListContainer.current = ref)}>
+      <ScrollView pagingEnabled contentContainerStyle={styles.scrollView} horizontal>
+        {wordsList.map(word => (
+          <WordCard key={word} word={word} width={viewWidth} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {

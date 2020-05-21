7559c9fe440ef23d1967a50eeda630026e7990f2
David Fudge <rkstar@mac.com>
2020-05-21 16:06:38 -0400

    add horizontal scrolling word list (#6)

--- src/app/WordCard.tsx
+++ src/app/WordCard.tsx
@@ -8,16 +8,22 @@ interface Props {
 }
 
 const WordCard = ({word}: Props) => (
-  <View style={styles.card}>
-    <Card elevation={4}>
-      <CardContent>
-        <Text style={styles.text}>{word}</Text>
-      </CardContent>
-    </Card>
+  <View style={styles.cardView}>
+    <View style={styles.card}>
+      <Card elevation={4}>
+        <CardContent>
+          <Text style={styles.text}>{word}</Text>
+        </CardContent>
+      </Card>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
+  cardView: {
+    width: '100%',
+    alignItems: 'center',
+  },
   card: {
     width: '60%',
     textAlign: 'center',

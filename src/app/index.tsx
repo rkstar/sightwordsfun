e001ef4115eb30769952983e27836d6ac5f5960f
David Fudge <rkstar@mac.com>
2020-05-22 07:09:38 -0400

    rolling scrolling (#9)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,21 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {View, ScrollView, StyleSheet} from 'react-native';
+import React, {useRef, useEffect, useState, useCallback} from 'react';
+import {View, ScrollView, StyleSheet, NativeSyntheticEvent, NativeScrollEvent} from 'react-native';
 
 import WordCard from './WordCard';
 
 const wordsList = ['one', 'two', 'three', 'box', 'mix', 'fox', 'up', 'are', 'four', 'five', 'lunch'];
+type DataDecision = 1 | -1 | 0;
+const decideData = (viewWidth: number, contentWidth: number, scrollPosition: number): DataDecision => {
+  if (scrollPosition >= contentWidth - viewWidth) return 1;
+  if (scrollPosition < viewWidth) return -1;
+  return 0;
+};
 
 const App = () => {
   const [viewWidth, setViewWidth] = useState<number>(0);
+  const [data, setData] = useState<string[]>(wordsList);
   const wordListContainer = useRef<View | null>();
+  const scrollViewRef = useRef<ScrollView | null>();
 
   useEffect(() => {
     const measureView = () => wordListContainer.current?.measure((_, __, width) => setViewWidth(width));
@@ -16,11 +24,39 @@ const App = () => {
     return () => window.removeEventListener('resize', measureView);
   }, []);
 
+  const onScroll = useCallback(
+    ({
+      nativeEvent: {
+        contentSize: {width},
+        contentOffset: {x},
+      },
+    }: NativeSyntheticEvent<NativeScrollEvent>) => {
+      const direction = decideData(viewWidth, width, x);
+      if (direction === 1) setData(current => current.concat(wordsList));
+      if (direction === -1) {
+        setData(current => wordsList.concat(current));
+        scrollViewRef.current?.scrollTo({x: viewWidth * wordsList.length, animated: false});
+      }
+    },
+    [viewWidth],
+  );
+
   return (
     <View style={styles.container} ref={ref => (wordListContainer.current = ref)}>
-      <ScrollView pagingEnabled contentContainerStyle={styles.scrollView} horizontal>
-        {wordsList.map(word => (
-          <WordCard key={word} word={word} width={viewWidth} />
+      <ScrollView
+        ref={ref => (scrollViewRef.current = ref)}
+        horizontal
+        directionalLockEnabled
+        disableIntervalMomentum
+        removeClippedSubviews
+        pagingEnabled
+        decelerationRate="fast"
+        contentContainerStyle={styles.scrollView}
+        onScroll={onScroll}
+        scrollEventThrottle={1000}
+      >
+        {data.map((word, index) => (
+          <WordCard key={`${index.toString()}_${word}`} word={word} width={viewWidth} />
         ))}
       </ScrollView>
     </View>
